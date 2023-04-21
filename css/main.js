5e4458e8c28f62c43cfaf2321f7b98dd3806ed53
badakaya <mydodream87@gmail.com>
2023-04-21 23:14:56 +0900

    updated

--- css/main.js
+++ css/main.js
@@ -20,7 +20,28 @@ faqs.forEach(faq => {
             icon.className = "material-symbols-outlined";
         } else {
             icon.className = "material-symbols-outlined";
-            
+
         }
     })
 })
+
+
+// show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
