2e673c8464e425094698fa0c3ed0e701aa3fff92
badakaya <mydodream87@gmail.com>
2023-04-21 21:11:43 +0900

    3times

---
+++ css/main.js
@@ -0,0 +1,26 @@
+// Change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'material-symbols-outlined') {
+            icon.className = "material-symbols-outlined";
+        } else {
+            icon.className = "material-symbols-outlined";
+            
+        }
+    })
+})
